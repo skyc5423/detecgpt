369aa00f613813d85229f36bcbc0cc44b57d1bb2
sangmin-lee-nuvi <sangmin.lee@nuvilabs.com>
2023-11-18 14:07:20 +0900

    added text output

--- projects/detect_gpt/app.py
+++ projects/detect_gpt/app.py
@@ -11,20 +11,27 @@ import os
 
 api_key = os.environ['OPENAI_API_KEY']
 
-def inference(x: np.ndarray):
-    # Resize along the long length and padding to 512, 512 to match the size.
-    img = Image.fromarray(x)
+
+def preprocess_img(img):
+    img = Image.fromarray(img)
     img = resize_long(img, 512)
     original_size = img.size
     img = pad_to_square(img, 512)
+    return img, original_size
 
+
+def request_gpt(img):
     headers, payload = get_payload(pil_to_base64(img), prompt_dish_multiple)
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
     content = response.json()['choices'][0]['message']['content']
-    print(content)
     output = json.loads(''.join(content.split('\n')[1:-1]))
 
+    return output
+
+
+def draw_bbox(img, output):
+    # draw bbox and make coco style annotation
+    annotations = {}
     draw = ImageDraw.Draw(img)
     for obj in output['foods']:
         bboxes = inferencer.predict_dino(img, obj)
@@ -35,23 +42,30 @@ def inference(x: np.ndarray):
             if isinstance(obj, dict):
                 obj = obj['name']
             draw.text(bbox, obj, (r, g, b))
+            annotations[obj] = bbox
 
+    return img, annotations
+
+def inference(x: np.ndarray):
+    img, original_size = preprocess_img(x)
+    output = request_gpt(img)
+    img, coco = draw_bbox(img, output)
     img = crop_to_original(img, original_size)
-
-    return np.array(img)
+    return np.array(img), coco
 
 
 if __name__ == '__main__':
     inferencer = GDinoInferencer('cpu')
 
     with gr.Blocks() as demo:
-        gr.Markdown("Flip text or image files using this demo.")
         with gr.Row():
             image_input = gr.Image(height=512, width=512)
-        with gr.Row():
             image_output = gr.Image(height=512, width=512)
+        with gr.Row():
+            text_output = gr.Textbox(lines=5, label="Output")
+
         image_button = gr.Button("Detect")
 
-        image_button.click(inference, inputs=image_input, outputs=image_output)
+        image_button.click(inference, inputs=image_input, outputs=[image_output, text_output])
 
     demo.launch()
